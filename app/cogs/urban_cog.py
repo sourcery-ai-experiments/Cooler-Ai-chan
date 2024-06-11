b6eadd5432074d037ec32158d5b85ebb28691d7c
Madrus <67657497+ShiroePL@users.noreply.github.com>
2024-06-11 20:10:30 +0200

    🔧 Add Urban Module with command to fetch urban dictionary definition

---
+++ app/cogs/urban_cog.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import aiohttp
+from app.utils.logger import logger
+from app.utils.embeds import get_urban_embed
+from app.services.database_service import DatabaseService
+
+class UrbanModule(commands.Cog):
+    """Services offered by Ai-Chan - paid 1 exp per use"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.database = DatabaseService()
+        self.get_urban_embed = get_urban_embed
+
+    @commands.command(name='urban', help="urban dictionary")
+    async def get_urban_dictionary_definition(self, ctx, *, term: str):
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(f"https://api.urbandictionary.com/v0/define?term={term}") as response:
+                    if response.status != 200:
+                        await ctx.send("Failed to fetch data from Urban Dictionary.")
+                        return
+
+                    data = await response.json()
+                    results = data.get('list', [])
+
+                    if not results:
+                        await ctx.send("Nothing's here.")
+                        return
+
+                    page_size = 2  # Number of definitions to display per page
+                    results = results[:page_size]
+
+                    embed = self.get_urban_embed(term, results)
+                    message = await ctx.send(embed=embed)
+                    custom_emoji = "<:kiana:1093963437522038936>"
+
+                    try:
+                        await message.add_reaction(custom_emoji)
+                    except discord.HTTPException:
+                        logger.error(f"Unknown Emoji: {custom_emoji}")
+            except Exception as ex:
+                logger.error(f"Error: {ex}")
+                await ctx.send("An error occurred while processing your request.")
+
+async def setup(bot):
+    await bot.add_cog(UrbanModule(bot))
