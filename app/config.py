6a072730564925e3f6544ff30f0c515009842d61
Madrus <67657497+ShiroePL@users.noreply.github.com>
2024-06-12 20:38:27 +0200

    Fixed paths and some variables
    Now bot is actualy starting

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     GUILD_ID = int(os.getenv('GUILD_ID'))
-    AI_GROQ_KEY = os.getenv('AI_GROQ_KEY')
+    ai_groq_key = os.getenv('AI_GROQ_KEY')
     LOG_FILE_PATH = os.getenv('LOG_FILE_PATH', 'app/persistent_data/logs/discord_bot.log')
     LOKI_URL = os.getenv('LOKI_URL', 'http://localhost:3100')
     PREFIX = os.getenv('PREFIX', '+')
