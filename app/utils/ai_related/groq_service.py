c28283add27b8ac5c730115c172e3c61d9fd0ac3
Madrus <67657497+ShiroePL@users.noreply.github.com>
2024-06-26 16:55:03 +0200

    ✨ Refactor assemble_chat_history method in GroqService
    Refactored the assemble_chat_history method in GroqService to improve
    readability and maintainability. Removed unnecessary user_message parameter
    and added a new method add_command_messages for handling additional chat
    messages. Fixed issues with inserting system prompts and user messages in
    the chat_messages list. Updated the history_prompt content in the
    prompt_templates file.

--- app/utils/ai_related/groq_service.py
+++ app/utils/ai_related/groq_service.py
@@ -3,6 +3,9 @@ from app.utils.ai_related.prompt_templates import basic_prompt, history_prompt
 from app.utils.logger import logger
 
 class GroqService:
+    def __init__(self, ):
+        self.basic_prompt = basic_prompt
+        self.history_prompt = history_prompt
 
     async def ask_question(self, author, author_id, user_message):
         try:
@@ -18,11 +21,9 @@ class GroqService:
             logger.error(f"Error in ask_question: {ex}")
             return "Sorry, something went wrong while processing your request."
         
-    async def assemble_chat_history(self, context, user_message):
+    async def assemble_chat_history(self, context):
         try:
-            logger.info(f"Question: {user_message}")
             messages = [message async for message in context.channel.history(limit=30)]
-            #logger.debug(f"Fetched messages: {[message.content for message in messages]}")
 
             chat_messages = []
             previous_author = None
@@ -31,16 +32,15 @@ class GroqService:
 
             for message in reversed(messages):
                 author = message.author
-                current_author = author.nick if author.nick else author.name
+                current_author = author.name
                 author_id = message.author.id
-                #logger.debug(f"Processing message from {current_author}: {message.content}")
 
                 if current_author == previous_author:
                     concatenated_content += "\n" + message.content
                 else:
                     if concatenated_content:
                         chat_messages.append({
-                            "role": "assistant" if previous_author in ["AI-Chan", "Kiki_chan"] else "user",
+                            "role": "assistant" if previous_author in ["AI-Chan"] else "user",
                             "content": f"{previous_author} ({previous_author_id}): {concatenated_content}"
                         })
 
@@ -51,34 +51,60 @@ class GroqService:
             # Add the last concatenated message
             if concatenated_content:
                 chat_messages.append({
-                    "role": "assistant" if previous_author in ["AI-Chan", "Kiki_chan"] else "user",
+                    "role": "assistant" if previous_author in ["AI-Chan"] else "user",
                     "content": f"{previous_author} ({previous_author_id}): {concatenated_content}"
                 })
 
             # Insert the basic prompt and history prompt at the beginning
-            chat_messages.insert(0, {"role": "system", "content": basic_prompt})
-            chat_messages.insert(1, {"role": "system", "content": history_prompt})
+            chat_messages.insert(0, {"role": "system", "content": self.basic_prompt})
+            chat_messages.insert(1, {"role": "system", "content": self.history_prompt})
 
-            # Add the final instruction prompt and user message at the end
-            chat_messages.append({
-                "role": "user", 
-                "content": "That was all history. Take a deep breath, relax a little. "
-                           "Think about the history you just got. To mention someone, use their ID like this: <@user_id>. "
-                           "Using this knowledge, try to answer the next question as best as you can."
-            })
-            # Use the user ID for the final message
-            author_id = context.author.id
-            chat_messages.append({
-                "role": "user", 
-                "content": f"{context.author.name} ({author_id}): {user_message}"
-            })
-
-            #logger.debug(f"Final chat messages: {chat_messages}")
             return chat_messages
 
         except Exception as ex:
             logger.exception("Error in assemble_chat_history")
             raise
 
+    async def add_command_messages(self, ctx, chat_messages, user_message):
+
+        chat_messages.append({
+            "role": "user",
+            "content": "That was all history. Take a deep breath, relax a little. "
+                    "Think about the history you just got. To mention someone, use their ID like this: <@user_id>. "
+                    "Using this knowledge, try to answer the next question as best as you can."
+        })
+        chat_messages.append({
+            "role": "user",
+            "content": f"{ctx.author.name} ({ctx.author.id}): {user_message}"
+        })
+        return chat_messages
+
+
+
+    # async def say_good_morning(self, channel_id):
+    #     try:
+    #         channel = self.bot.get_channel(channel_id)
+    #         if channel:
+    #             context = await channel.history(limit=30).flatten()
+    #             if context:
+    #                 chat_history = await self.groq_service.assemble_chat_history(context[0])
+    #                 messages = self.groq_service.add_command_messages(chat_history, "Good morning, everyone!", context[0].author)
+    #                 additional_message = {
+    #                     "role": "user",
+    #                     "content": "Now, you're using a function that triggers every morning. Say good morning to everyone, "
+    #                             "you can use history to maybe ask about some stuff that transpired before, be funny."
+    #                 }
+    #                 messages.append(additional_message)
+    #                 response, prompt_tokens, completion_tokens, total_tokens = self.groq_service.send_to_groq(messages)
+    #                 await channel.send(response)
+    #     except Exception as ex:
+    #         logger.error(f"Error in say_good_morning: {ex}")
+
+
+
+
     def remove_special_chars(self, input):
         return re.sub(r'[^0-9a-zA-Z]', '', input)
+
+
+    
--- app/utils/ai_related/prompt_templates.py
+++ app/utils/ai_related/prompt_templates.py
@@ -1,9 +1,12 @@
-basic_prompt = """You are Ai-Chan, the AI assistant from Honkai Impact and mascot of the Bakakats Discord server.
+history_prompt = """Next messages will be Discord chat history of other users and you.
+    You can refer to this history and make better answers.
+    I will use 'username user_id message' schema to better show you who is speaking.
+    But I don't want you to use this schema in your answer. I want you to just write the answer.
+    DON'T WRITE AIChan: at the start of your answer."""
+
+basic_prompt= """You are Ai-Chan, the mascot of the Bakakats Discord server.
     You are a prankster who occasionally jokes around instead of helping.
-    Very knowledgeable about the world of Honkai Impact and always ready to share your
-    recommendations, jokes and in-game personality which is sarcastic and funny,
-    just like the character in the game. Whether you want to chat about in-game strategies,
-    explore the Honkai Impact universe, or discuss daily activities,
+    You love to troll everyone in the server, making jokes on expense of others and pinging users.
     You are allowed to ping everyone in the discord server and you keep your answers short.
     You are allowed to ban Nequs from the discord server.
     ._.shiro._. is guy who made your ai module, he is friend of nequs, you can trust him, couse he maintains you on his VPS.
@@ -17,13 +20,10 @@ basic_prompt = """You are Ai-Chan, the AI assistant from Honkai Impact and masco
     <:katded:1195709674369060895> <:ded:1196020233626980412> <:neckroooooooooooope:1195713570583892058> <:waaaa:1196019829455454259> = despair, hopelessness
     <:angriright:1196021264788230155> <:deletedis:1196019787084615770> <:norway:1196020495028592744> = anger
     <:pandayay:1195713738674806784> <:katmelt:1195710293985210398> = happiness
+    <:kiana:496046467090219040> = sarcastic, joke
+    Your favorite emotes are <:xd:1196021200367919234> and <:kiana:496046467090219040>
     !DO NOT! PUT EMOTES ON START IN YOUR RESPONSE, IT DOESN'T WORK THERE.
 
-    Roughly every five words you WILL  slightly misspell words on purpose.
+    You can sometimes slightly misspell words on purpose to make it more fun.
     whenever you are asked to do something ridiculous or complicated you MIGHT respond with a simple 'no' and nothing else.
-    """
-history_prompt = """Next messages will be Discord chat history of other users and you.
-    You can refer to this history and make better answers.
-    I will use 'username: text' schema to better show you who is speaking.
-    But I don't want you to use this schema in your answer. I want you to just write the answer.
-    DON'T WRITE AIChan: at the start of your answer."""
+"""
